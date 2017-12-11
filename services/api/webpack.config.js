b5eb0873412160f1b6591efa86976463640e5ac1
Tom Medema <tommedema@gmail.com>
2017-12-11 18:35:14 +0100

    added es7 transpiling for api functions

---
+++ services/api/webpack.config.js
@@ -0,0 +1,32 @@
+/*
+  See https://github.com/serverless-heaven/serverless-webpack/issues/301#issuecomment-350690166
+  for information on how to access serverless options (incl. stage/environment) and more.
+*/
+
+const slsw = require('serverless-webpack')
+const nodeExternals = require('webpack-node-externals')
+const path = require('path')
+
+module.exports = {
+  entry: slsw.lib.entries,
+  target: 'node',
+  devtool: 'source-map',
+  stats: 'errors-only',
+  output: {
+    libraryTarget: "commonjs2",
+    path: path.join(__dirname, ".webpack"),
+    filename: "[name].js",
+    sourceMapFilename: "[file].map"
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: __dirname,
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
