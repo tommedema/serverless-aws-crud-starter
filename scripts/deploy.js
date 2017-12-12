ce576c48b3e70f8678f5357c05a6e96438bf6233
Tom Medema <tommedema@gmail.com>
2017-12-12 01:09:29 +0100

    initial npm scripts

---
+++ scripts/deploy.js
@@ -0,0 +1,11 @@
+// determine if we should run this with babel-node
+// to support node 6.10.3 and make local debugging easier
+// or if we should force a newer version of node for development (> 8.9.3)
+
+// retrieve project name and default aws region from config or package.json
+
+// in each service, sorted alphabetically, run `npm run deploy` with env vars
+
+// consider setting engine requirements in mother package.json
+// then have each child service inherit from this
+// same for sub service name (project-subservice format)
