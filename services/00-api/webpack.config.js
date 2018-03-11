f08c3a2536480fc861b23a23e29a9525b97fd438
Tom Medema <tommedema@gmail.com>
2018-03-11 14:39:35 +0100

    do not minimize api code

--- services/00-api/webpack.config.js
+++ services/00-api/webpack.config.js
@@ -11,6 +11,9 @@ module.exports = {
   entry: slsw.lib.entries,
   target: 'node',
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  optimization: {
+		minimize: false
+	},
   devtool: 'source-map',
   stats: 'minimal',
   output: {
